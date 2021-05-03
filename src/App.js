260d3ae8fb21e5fcb91fda13a6ad76c6c6f4c179
Brandon Porcel <brandon7.7porcel@gmail.com>
2021-05-03 13:46:53 -0300

    agreue las rutas e instale rrdom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import Home from './components/Home';
+import Videos from './components/Videos';
+import Tour from './components/Tour';
+import Merch from './components/Merch';
+import SignUp from './components/SignUp';
 function App() {
-	return <div className="App"></div>;
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home}></Route>
+					<Route path="/videos" component={Videos}></Route>
+					<Route path="/tour">
+						<Tour></Tour>
+					</Route>
+					<Route path="/merch">
+						<Merch></Merch>
+						merchhhhhh mercha2
+					</Route>
+					<Route path="/signup" component={SignUp}></Route>
+				</Switch>
+			</Router>
+		</>
+	);
 }
 
 export default App;
